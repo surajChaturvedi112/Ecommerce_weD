6a59ff24faa3acf77fdfc3553b3c9412612d6797
suraj chaturvedi <surajchaturvedi1111@gmail.com>
2024-02-19 20:18:19 +0530

    adding login page

--- front-end/Front_End/src/components/Login.jsx
+++ front-end/Front_End/src/components/Login.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 
 const Login = () => {
     return (
         <div>
             <h1> This is Login</h1>
+            <Button sx={{mt:0,ml:3,height:40,width:100}} color="secondary" variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
         </div>
     );
 };
